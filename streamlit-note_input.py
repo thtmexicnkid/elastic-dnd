f4c8e8a88a50d34d01a7b36738eed52edfbc0ff2
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-08-22 16:58:54 -0400

    Update streamlit-note_input.py

--- streamlit-note_input.py
+++ streamlit-note_input.py
@@ -1,7 +1,16 @@
+import json
+import requests
 import streamlit
 
-streamlit.title("D&D Note Input")
+elastic_url = "" + index + "/_doc"
+elastic_headers = {"Content-Type":"application/json"}
+elastic_auth = HTTPBasicAuth("ApiKey","")
 
+def send_to_elastic(payload):
+    response = requests.post(elastic_url,data=payload,headers=elastic_headers,auth=elastic_auth)
+    return response.json
+
+streamlit.title("D&D Note Input")
 note_taker = streamlit.selectbox("Who are you?", ["Bengamin Bolton","Corver Flickerspring","Mae Avraya", "Nyx", "Tanja"])
 if note_taker == "Bengamin Bolton":
     index = "dnd-notes-bengamin_bolton"
@@ -13,13 +22,17 @@ elif note_taker == "Nyx":
     index = "dnd-notes-nyx"
 elif note_taker == "Tanja":
     index = "dnd-notes-tanja"
-#streamlit.text(index)
 
 type = streamlit.selectbox("What kind of note is this?", ["location","overview","person","quest"])
 session = streamlit.number_input("Which session is this?", 0, 250)
 message = streamlit.text_input("Input note here:")
 if type == "quest":
+    #query elastic for a list of quest names???
     name = streamlit.text_input("What is the name of the quest?")
     finished = streamlit.checkbox("Is the quest finished?")
-    streamlit.write([index, type, session, message, name, finished])
-streamlit.write([index, type, session, message])
+
+    json = json.dumps({"finished":finished,"message":message,"name":name,"session":session,"type":type})
+else:
+    json = json.dumps({"message":message,"session":session,"type":type})
+
+streamlit.button("Click to submit note.", on_click=send_to_elastic(json))
