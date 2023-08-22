a265e9c2d9810bf8b08e63733cf6bb758221087c
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-08-22 16:01:17 -0400

    Update streamlit-note_input.py

--- streamlit-note_input.py
+++ streamlit-note_input.py
@@ -1,4 +1,7 @@
 import streamlit
 
-streamlit.text('This is some random test text.')
-streamlit.selectbox('Pick log type:', ['location', 'overview', 'person', 'quest'])
+streamlit.title("D&D Note Input")
+note_taker = streamlit.selectbox('Who are you?', ['Bengamin Bolton','Corver Flickerspring','Mae Avraya', 'Nyx', 'Tanja'])
+
+if note_taker == "Bengamin Bolton":
+  index = "dnd-notes-bengamin_bolton"
