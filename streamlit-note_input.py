61652d9be4f96cf7428e355296d00ead3c6d0bc2
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-08-22 16:05:09 -0400

    Update streamlit-note_input.py

--- streamlit-note_input.py
+++ streamlit-note_input.py
@@ -5,4 +5,13 @@ note_taker = streamlit.selectbox('Who are you?', ['Bengamin Bolton','Corver Flic
 
 if note_taker == "Bengamin Bolton":
   index = "dnd-notes-bengamin_bolton"
-  streamlit.text(index)
+elif note_taker == "Corver Flickerspring":
+  index = "dnd-notes-corver_flickerspring"
+elif note_taker == "Mae Avraya":
+  index = "dnd-notes-mae_avraya"
+elif note_taker == "Nyx":
+index = "dnd-notes-nyx"
+elif note_taker == "Tanja":
+  index = "dnd-notes-tanja"
+
+streamlit.text(index)
