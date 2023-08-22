ea0fd762201b18724c0dd76ef501a276127d167e
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-08-22 16:23:31 -0400

    Update streamlit-note_input.py

--- streamlit-note_input.py
+++ streamlit-note_input.py
@@ -1,24 +1,25 @@
 import streamlit
 
 streamlit.title("D&D Note Input")
-note_taker = streamlit.selectbox('Who are you?', ['Bengamin Bolton','Corver Flickerspring','Mae Avraya', 'Nyx', 'Tanja'])
+
+note_taker = streamlit.selectbox("Who are you?", ["Bengamin Bolton","Corver Flickerspring","Mae Avraya", "Nyx", "Tanja"])
 if note_taker == "Bengamin Bolton":
-  index = "dnd-notes-bengamin_bolton"
+    index = "dnd-notes-bengamin_bolton"
 elif note_taker == "Corver Flickerspring":
-  index = "dnd-notes-corver_flickerspring"
+    index = "dnd-notes-corver_flickerspring"
 elif note_taker == "Mae Avraya":
-  index = "dnd-notes-mae_avraya"
+    index = "dnd-notes-mae_avraya"
 elif note_taker == "Nyx":
-  index = "dnd-notes-nyx"
+    index = "dnd-notes-nyx"
 elif note_taker == "Tanja":
-  index = "dnd-notes-tanja"
+    index = "dnd-notes-tanja"
 #streamlit.text(index)
-note_type = streamlit.selectbox('What kind of note is this?', ['location','overview','person','quest'])
-if note_type == "location":
-  streamlit.number_input('Which session is this?', 0, 250)
-elif note_type == "overview":
-  continue
-elif note_type == "person":
-  continue
-elif note_type == "quest":
-  continue
+
+type = streamlit.selectbox("What kind of note is this?", ["location","overview","person","quest"])
+session = streamlit.number_input("Which session is this?", 0, 250)
+message = streamlit.text_input("Input note here:")
+if note_type == "quest":
+    name = streamlit.text_input("What is the name of the quest?")
+    finished = streamlit.checkbox("Is the quest finished?")
+
+streamlit.write([index, type, session, message, name, finished])
