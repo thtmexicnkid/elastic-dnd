c9e4a9a81d8c4efb0cb2ca78d87ff0fd60bedd21
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-08-28 10:19:50 -0400

    Update streamlit-note_input.py

--- streamlit-note_input.py
+++ streamlit-note_input.py
@@ -1,23 +1,12 @@
-# thtmexicnkid
-# Streamlit: D&D Note Input
-# Author: Joe M
-# Last Updated: Aug 22nd, 2023
-# 
-# Creates GUI for note input for D&D via Elastic
-
-import json
 import streamlit as st
 import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
 
-character_to_index = {"Bengamin Bolton":"dnd-notes-bengamin_bolton","Corver Flickerspring":"dnd-notes-corver_flickerspring","Mae Avraya":"dnd-notes-mae_avraya", "Nyx":"dnd-notes-nyx", "Tanja":"dnd-notes-tanja"}
 elastic_url = "https://localhost:9200"
-elastic_ca_certs = "PATH TO ELASTIC CA CERT"
-elastic_api_key = ("API_KEY_ID","API_KEY_VALUE")
-streamlit_path = "PATH TO STREAMLIT FILE"
+elastic_ca_certs = "D:\\Games\\DnD\\Notes\\kibana-8.8.2\\data\\ca_1690065405072.crt"
+elastic_api_key = ("2yMSIIoBnQzsNJ6e4j88","ApjPdeDkRhWtkusfC55qHw")
+streamlit_path = "D:\\Games\\DnD\\Notes\\"
 
-def app_page1():
+def app_page1():    
     tab1, tab2 = st.tabs(["Login", "Register"])
     with tab1:
         try:
@@ -42,56 +31,90 @@ def app_page1():
 def app_page2():
     tab1, tab2 = st.tabs(["Note Input", "WIP - Ask Virtual DM"])
     with tab1:
-        st.session_state["note_type"] = st.selectbox("Audio or Text?", ["Audio","Text"])
-        if st.session_state["note_type"] == "Audio":
-            log_index = "dnd-notes-transcribed"
-            log_type = "audio"
-            with st.form("audio_form", clear_on_submit=True):
-                log_session = st.number_input("Which session is this?", 0, 250)
-                file = st.file_uploader("Choose audio file",type=["mp3"])
-                submitted = st.form_submit_button("Upload file")
-                if submitted and file is not None:
-                    transcribed_text = transcribe_audio(file.name)
-                    if transcribed_text is not None:
-                        log_payload = json.dumps({"session":log_session,"type":log_type,"message":transcribed_text})
-                        elastic_index_document(log_index,log_payload)
-                    else:
-                        st.error("Audio transcription failure")
-                else:
-                    st.warning('Please upload a file and submit')
-        elif st.session_state["note_type"] == "Text":
-            log_type = st.selectbox("What kind of note is this?", ["location","overview","person","quest"])
-            if log_type == "quest":
-                with st.form("text_form_w_quest", clear_on_submit=True):
-                    log_session = st.number_input("Which session is this?", 0, 250)
-                    note_taker = st.selectbox("Who are you?", ["Bengamin Bolton","Corver Flickerspring","Mae Avraya", "Nyx", "Tanja"])
-                    log_index = character_to_index[note_taker]
-                    #I want to change this to a dropdown box populated with a list of available quest names from Elastic, if possible
-                    quest_name = st.text_input("What is the name of the quest?")
-                    quest_finished = st.checkbox("Is the quest finished?")
-                    log_message = st.text_input("Input note text:")
-                    submitted = st.form_submit_button("Upload note")
-                    if submitted is not None:
-                        log_payload = json.dumps({"finished":quest_finished,"message":log_message,"name":quest_name,"session":log_session,"type":log_type})
-                        elastic_index_document(log_index,log_payload)
-            else:
-                with st.form("text_form_wo_quest", clear_on_submit=True):
-                    log_session = st.number_input("Which session is this?", 0, 250)
-                    note_taker = st.selectbox("Who are you?", ["Bengamin Bolton","Corver Flickerspring","Mae Avraya", "Nyx", "Tanja"])
-                    log_index = character_to_index[note_taker]
-                    log_message = st.text_input("Input note text:")
-                    submitted = st.form_submit_button("Upload Note")
-                    if submitted is not None:
-                        log_payload = json.dumps({"message":log_message,"session":log_session,"type":log_type})
-                        elastic_index_document(log_index,log_payload)
+        st.session_state["note_type"] = st.selectbox("Audio or Text?", ["Audio","Text"], index=0)
+        
+        if st.session_state.note_type == "Audio":
+            st.session_state.runpage = app_page2_audio
+            st.session_state.runpage()
+            if st.session_state == {}:
+                st.session_state.runpage = app_page2_audio
+                st.experimental_rerun()
+        elif st.session_state.note_type == "Text":
+            st.session_state.runpage = app_page2_text
+            st.session_state.runpage()
+            if st.session_state == {}:
+                st.session_state.runpage = app_page2_text
+                st.experimental_rerun()
+        else:
+            pass
     with tab2:
         #I want to run notes through AI to be able to ask questions from this tab and receive answers
         pass
+    
     authenticator.logout('Logout', 'main')
+    
+def app_page2_audio():
+    import json
+    
+    audio_form_variable_list = ["log_index","log_type","log_session","file","submitted","transcribed_text","log_payload"]
+    
+    with st.form("audio_form", clear_on_submit=True):
+        st.session_state["log_index"] = "dnd-notes-transcribed"
+        st.session_state["log_type"] = "audio"
+        st.session_state["log_session"] = st.slider("Which session is this?", 0, 250)
+        st.session_state["file"] = st.file_uploader("Choose audio file",type=["mp3"])
+        st.session_state["submitted"] = st.form_submit_button("Upload file")
+
+        if st.session_state.submitted and st.session_state.file is not None:
+            #st.session_state["transcribed_text"] = transcribe_audio(st.session_state.file.name)
+            st.session_state["transcribed_text"] = "Test"
+            if st.session_state.transcribed_text is not None:
+                st.session_state["log_payload"] = json.dumps({"session":st.session_state.log_session,"type":st.session_state.log_type,"message":st.session_state.transcribed_text})
+                elastic_index_document(st.session_state.log_index,st.session_state.log_payload)
+            else:
+                st.error("Audio transcription failure")
+        else:
+            st.warning('Please upload a file and submit')
+    
+    clear_session_state(audio_form_variable_list)
+    
+def app_page2_text():
+    import json
+    
+    text_form_variable_list = []
+    """
+    log_type = st.selectbox("What kind of note is this?", ["location","overview","person","quest"])
+    if log_type == "quest":
+        with st.form("text_form_w_quest", clear_on_submit=True):
+            log_session = st.number_input("Which session is this?", 0, 250)
+            note_taker = st.selectbox("Who are you?", ["Bengamin Bolton","Corver Flickerspring","Mae Avraya", "Nyx", "Tanja"])
+            log_index = character_to_index[note_taker]
+            #I want to change this to a dropdown box populated with a list of available quest names from Elastic, if possible
+            quest_name = st.text_input("What is the name of the quest?")
+            quest_finished = st.checkbox("Is the quest finished?")
+            log_message = st.text_input("Input note text:")
+            submitted = st.form_submit_button("Upload note")
+            if submitted is not None:
+                log_payload = json.dumps({"finished":quest_finished,"message":log_message,"name":quest_name,"session":log_session,"type":log_type})
+                elastic_index_document(log_index,log_payload)
+    else:
+        with st.form("text_form_wo_quest", clear_on_submit=True):
+            log_session = st.number_input("Which session is this?", 0, 250)
+            note_taker = st.selectbox("Who are you?", ["Bengamin Bolton","Corver Flickerspring","Mae Avraya", "Nyx", "Tanja"])
+            log_index = character_to_index[note_taker]
+            log_message = st.text_input("Input note text:")
+            submitted = st.form_submit_button("Upload Note")
+            if submitted is not None:
+                log_payload = json.dumps({"message":log_message,"session":log_session,"type":log_type})
+                elastic_index_document(log_index,log_payload)
+    """
 
 def clear_session_state(variable_list):
     for variable in variable_list:
-        del st.session_state[variable]
+        try:
+            del st.session_state[variable]
+        except:
+            pass
 
 def elastic_index_document(index,document):
     from elasticsearch import Elasticsearch
@@ -112,7 +135,6 @@ def elastic_index_document(index,document):
         st.error("Note creation failure")
     
     client.close()
-    response = None
 
 def initialize_session_state(variable_list):
     for variable in variable_list:
@@ -120,6 +142,9 @@ def initialize_session_state(variable_list):
             st.session_state[variable] = None
 
 def load_yml():
+    import yaml
+    from yaml.loader import SafeLoader
+    
     with open(streamlit_path + "auth_config.yaml") as file:
         config = yaml.load(file, Loader=SafeLoader)
 
@@ -150,6 +175,8 @@ def transcribe_audio(file):
     return transcript.text
 
 def update_yml():
+    import yaml
+    
     with open(streamlit_path + "auth_config.yaml", 'w') as file:
         yaml.dump(config, file, default_flow_style=False)
 
