0f7503e73caf0422b6ca728dbd8e6e1029155358
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-09-01 14:48:25 -0400

    Update generate_password.py

--- generate_password.py
+++ generate_password.py
@@ -1,5 +1,4 @@
-import streamlit as st
 import streamlit_authenticator as stauth
 
 hashed_passwords = stauth.Hasher(["YOUR_PASSWORD"]).generate()
-st.text(hashed_passwords)
+print(hashed_passwords)
