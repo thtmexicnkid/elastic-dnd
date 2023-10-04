2bdf536a13aee399b090befcfb64af6a0ed05ab4
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-10-03 21:39:01 -0400

    Create variables.py
    added streamlit page functionality

---
+++ project/streamlit/variables.py
@@ -0,0 +1,18 @@
+# Elastic D&D
+# Author: thtmexicnkid
+# Last Updated: 10/03/2023
+# 
+# Streamlit - Backend - Houses variables that are loaded into pages of the application.
+
+### VARIABLES ###
+# *** change this to fit your environment ***
+assemblyai_api_key = "API_KEY"
+elastic_api_key = "API_KEY"
+
+# *** DO NOT CHANGE ***
+elastic_url = "https://es01:9200"
+elastic_ca_certs = "certs/ca/ca.crt"
+fastapi_url = "http://api:8000"
+kibana_url = "http://kibana:5601"
+streamlit_data_path = "data/"
+streamlit_project_path = "streamlit/"
