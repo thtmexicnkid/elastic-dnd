93f9d5b098adcb84161ec365eefaa42e6d8817bf
thtmexicnkid <54283766+thtmexicnkid@users.noreply.github.com>
2023-11-03 17:10:47 -0400

    Update 1_Note_Input.py
    added logic for free vs. paid audio transcription

--- project/streamlit/pages/1_Note_Input.py
+++ project/streamlit/pages/1_Note_Input.py
@@ -1,12 +1,14 @@
 # Elastic D&D
 # Author: thtmexicnkid
-# Last Updated: 10/04/2023
+# Last Updated: 11/03/2023
 # 
 # Streamlit - Note Input Page - Allows the user to store audio or text notes in Elasticsearch.
 
 import streamlit as st
 from functions import *
 from variables import *
+
+st.session_state
 
 # displays application title and sets page accordingly
 display_image(streamlit_data_path + "banner.png")
@@ -27,28 +29,52 @@ else:
         #list of variables to clear from session state once finished
         audio_form_variable_list = ["log_type","log_session","file","submitted","transcribed_text","log_payload","message_vector"]
         
-        # displays note form widgets, creates note payload, sends payload to an Elastic index, and handles error / success / warning messages
-        with st.form("audio_form", clear_on_submit=True):
-            st.session_state["log_type"] = "audio"
-            st.session_state["log_session"] = st.slider("Which session is this?", 0, 250)
-            st.session_state["file"] = st.file_uploader("Choose audio file",type=[".3ga",".8svx",".aac",".ac3",".aif",".aiff",".alac",".amr",".ape",".au",".dss",".flac",".flv",".m2ts",".m4a",".m4b",".m4p",".m4p",".m4r",".m4v",".mogg",".mov",".mp2",".mp3",".mp4",".mpga",".mts",".mxf",".oga",".ogg",".opus",".qcp",".ts",".tta",".voc",".wav",".webm",".wma",".wv"])
-            st.session_state["submitted"] = st.form_submit_button("Upload file")
-            if st.session_state.submitted and st.session_state.file is not None:
-                # removes forward slash that will break the API call for AI functionality
-                st.session_state["transcribed_text"] = text_cleanup(transcribe_audio(st.session_state.file))
-                if st.session_state.transcribed_text is not None:
-                    # gets vector object for use with AI functionality
-                    st.session_state["message_vector"] = api_get_vector_object(st.session_state.transcribed_text)
-                    if st.session_state.message_vector == None:
-                        error_message("AI API vectorization failure",2)
+        if assemblyai_api_key:
+            # displays note form widgets, creates note payload, sends payload to an Elastic index, and handles error / success / warning messages
+            with st.form("audio_form", clear_on_submit=True):
+                st.session_state["log_type"] = "audio"
+                st.session_state["log_session"] = st.slider("Which session is this?", 0, 250)
+                st.session_state["file"] = st.file_uploader("Choose audio file",type=[".3ga",".8svx",".aac",".ac3",".aif",".aiff",".alac",".amr",".ape",".au",".dss",".flac",".flv",".m2ts",".m4a",".m4b",".m4p",".m4p",".m4r",".m4v",".mogg",".mov",".mp2",".mp3",".mp4",".mpga",".mts",".mxf",".oga",".ogg",".opus",".qcp",".ts",".tta",".voc",".wav",".webm",".wma",".wv"])
+                st.session_state["submitted"] = st.form_submit_button("Upload file")
+                if st.session_state.submitted and st.session_state.file is not None:
+                    # removes forward slash that will break the API call for AI functionality
+                    st.session_state["transcribed_text"] = text_cleanup(transcribe_audio_paid(st.session_state.file))
+                    if st.session_state.transcribed_text not in (None,""):
+                        # gets vector object for use with AI functionality
+                        st.session_state["message_vector"] = api_get_vector_object(st.session_state.transcribed_text)
+                        if st.session_state.message_vector == None:
+                            error_message("AI API vectorization failure",2)
+                            st.session_state
+                        else:
+                            st.session_state["log_payload"] = json.dumps({"session":st.session_state.log_session,"type":st.session_state.log_type,"message":st.session_state.transcribed_text,"message_vector":st.session_state.message_vector})
+                            elastic_index_document("dnd-notes-transcribed",st.session_state.log_payload,True)
                     else:
-                        st.session_state["log_payload"] = json.dumps({"session":st.session_state.log_session,"type":st.session_state.log_type,"message":st.session_state.transcribed_text,"message_vector":st.session_state.message_vector})
-                        elastic_index_document("dnd-notes-transcribed",st.session_state.log_payload,True)
+                        error_message("Audio transcription failure",2)
                 else:
-                    error_message("Audio transcription failure",2)
-            else:
-                st.warning('Please upload a file and submit')
-        
+                    st.warning('Please upload a file and submit')
+        else:
+            # displays note form widgets, creates note payload, sends payload to an Elastic index, and handles error / success / warning messages
+            with st.form("audio_form", clear_on_submit=True):
+                st.session_state["log_type"] = "audio"
+                st.session_state["log_session"] = st.slider("Which session is this?", 0, 250)
+                st.session_state["file"] = st.file_uploader("Choose audio file",type=[".wav"])
+                st.session_state["submitted"] = st.form_submit_button("Upload file")
+                if st.session_state.submitted and st.session_state.file is not None:
+                    # removes forward slash that will break the API call for AI functionality
+                    st.session_state["transcribed_text"] = text_cleanup(transcribe_audio_free(st.session_state.file))
+                    if st.session_state.transcribed_text not in (None,""):
+                        # gets vector object for use with AI functionality
+                        st.session_state["message_vector"] = api_get_vector_object(st.session_state.transcribed_text)
+                        if st.session_state.message_vector == None:
+                            error_message("AI API vectorization failure",2)
+                        else:
+                            st.session_state["log_payload"] = json.dumps({"session":st.session_state.log_session,"type":st.session_state.log_type,"message":st.session_state.transcribed_text,"message_vector":st.session_state.message_vector})
+                            elastic_index_document("dnd-notes-transcribed",st.session_state.log_payload,True)
+                    else:
+                        error_message("Audio transcription failure",2)
+                else:
+                    st.warning('Please upload a file and submit')
+
         # clears session state
         clear_session_state(audio_form_variable_list)
     elif st.session_state.note_type == "Text":
